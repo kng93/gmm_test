9cf20429244d3d464f181254c14b2dd2b72c40de
smile547 <karin.ng@mail.utoronto.ca>
2017-04-25 16:56:37 -0400

    Started looking at EM for OpenCV

--- gmm/gmm/main.cpp
+++ gmm/gmm/main.cpp
@@ -1,25 +1,34 @@
 #include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
 #include <iostream>
+#include <opencv2/ml.hpp>
+
 using namespace cv;
-using namespace std;
+
+//#include <opencv2/imgcodecs.hpp>
+//#include <opencv2/highgui.hpp>
+//using namespace std;
+
 int main(int argc, char** argv)
 {
 	if (argc != 2)
 	{
-		cout << " Usage: display_image ImageToLoadAndDisplay" << endl;
+		std::cout << " Usage: gmm fileWithPoints" << std::endl;
 		return -1;
 	}
-	Mat image;
-	image = imread(argv[1], IMREAD_COLOR); // Read the file
-	if (image.empty()) // Check for invalid input
-	{
-		cout << "Could not open or find the image" << std::endl;
-		return -1;
-	}
-	namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
-	imshow("Display window", image); // Show our image inside it.
-	waitKey(0); // Wait for a keystroke in the window
-	return 0;
+
+	int k = 3;
+	Mat samples;
+
+	// TODO: Get data
+
+
+	// Train expectation maximization
+	Ptr<ml::EM> mdl = ml::EM::create();
+	mdl->setClustersNumber(k);
+	mdl->trainEM(samples);
+
+	// Get means and covariance matrix
+	Mat means = mdl->getMeans();
+	std::vector<Mat> cov;
+	mdl->getCovs(cov);
 }
