863cdba27e491656e4da54f309a17d1e5382877f
kng93 <karin.ng@mail.utoronto.ca>
2017-05-02 18:41:13 -0400

    OpenCV GMM EM working

--- gmm/gmm/main.cpp
+++ gmm/gmm/main.cpp
@@ -1,34 +1,72 @@
 #include <opencv2/core.hpp>
-#include <iostream>
 #include <opencv2/ml.hpp>
+//#include <opencv2/imgcodecs.hpp>
+#include <iostream>
+#include <string>
+#include <fstream>
 
 using namespace cv;
+#define NUM_CLUS 5
 
-//#include <opencv2/imgcodecs.hpp>
+
 //#include <opencv2/highgui.hpp>
-//using namespace std;
+int dataFromFile(std::string fn, Mat& samples)
+{
+	std::vector<float> val;
+	double raw;
+
+	// Get the data from the file
+	std::ifstream dFile(fn);
+	if (!dFile.is_open())
+		return -1;
+
+	while (dFile >> raw)
+		val.push_back(raw);
+
+	dFile.close();
+
+	// Put the data into the matrix
+	int num_points = val.size();
+	samples = Mat::zeros(num_points, 1, CV_64FC1);
+	for (int i = 0; i < num_points; i++)
+		samples.at<double>(i) = val[i];
+
+	return 0;
+}
+
 
 int main(int argc, char** argv)
 {
-	if (argc != 2)
-	{
-		std::cout << " Usage: gmm fileWithPoints" << std::endl;
-		return -1;
-	}
+	// Get the data from a file
+	std::string dataFile("../../setup/gmm.txt"); // by default
+	if (argc > 1)
+		dataFile = argv[1];
 
-	int k = 3;
+	// Get data
 	Mat samples;
+	if (dataFromFile(dataFile, samples) < 0)
+		return -1; // File didn't read properly
 
-	// TODO: Get data
-
-
-	// Train expectation maximization
+	
+	//// Train expectation maximization
 	Ptr<ml::EM> mdl = ml::EM::create();
-	mdl->setClustersNumber(k);
+	mdl->setClustersNumber(NUM_CLUS);
 	mdl->trainEM(samples);
 
-	// Get means and covariance matrix
+	// Get + print means
 	Mat means = mdl->getMeans();
+	std::cout << "Means: " << std::endl;
+	for (int i = 0; i < NUM_CLUS; i++)
+		std::cout << "\t" << means.at<double>(i) << std::endl;
+
+	// Get + print covariance matrix
 	std::vector<Mat> cov;
+	for (int i = 0; i < NUM_CLUS; i++)
+		cov.push_back(Mat::zeros(1, 1, CV_64FC1));
 	mdl->getCovs(cov);
+	std::cout << "\n\nSigmas: " << std::endl;
+	for (int i = 0; i < NUM_CLUS; i++)
+		std::cout << "\t" << cov[i].at<double>(0) << std::endl;
+
+	system("pause");
 }
---
+++ setup/genGMM.m
@@ -0,0 +1,30 @@
+data_range=[-1000,1000];
+sigma_range=[100,300];
+data_size=2000;
+n_gaussians=5;
+
+weights=rand(n_gaussians,1);
+weights = weights./sum(weights);
+means=zeros(n_gaussians,1);
+sigmas=zeros(1,1,n_gaussians);
+for i=1:n_gaussians
+    means(i)=data_range(1)+(data_range(2)-data_range(1))*rand(1);
+    sigmas(1,1,i)=sigma_range(1)+(sigma_range(2)-sigma_range(1))*rand(1);
+end
+gmm=gmdistribution(means,sigmas,weights);
+X=(data_range(1):1:data_range(2)).';
+Y=pdf(gmm,X);
+figure(10),plot(X,Y); title('True probability density function of the GMM');
+
+fileID = fopen('gmm.txt','w');
+sampled_data = random(gmm,data_size);
+for i=1:data_size
+    fprintf(fileID,'%f\n',sampled_data(i));
+end
+fclose(fileID);
+figure(20), hist(sampled_data,1000); title('Histogram of data sampled from the GMM');
+
+disp(means);
+disp(sigmas);
+
+
